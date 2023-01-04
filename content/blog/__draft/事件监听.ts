cdbf1d24a73c638bb6dfc5d49eb6db8a134234ce
liuweiyibai <lw1140@163.com>
2023-01-04 21:18:01 +0800

    docs: 更新文档

---
+++ content/blog/__draft/事件监听.ts
@@ -0,0 +1,16 @@
+class mitt {
+  events: {}
+  constructor() {
+    this.events = {}
+  }
+
+  emit(key: string) {}
+
+  on() {}
+
+  once() {}
+
+  remove() {}
+}
+
+// https://segmentfault.com/a/1190000041526820
